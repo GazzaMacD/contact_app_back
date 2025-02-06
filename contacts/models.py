99bade924c4ef812949a74dde77c691f44ecae95
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-02-06 16:25:39 +0900

    Change contact model and migrate

--- contacts/models.py
+++ contacts/models.py
@@ -9,35 +9,37 @@ class Contact(TimeStampedModel):
     Base Model for contacts to be stored in
     """
 
-    name = models.CharField(
-        _("Name"),
+    full_name = models.CharField(
+        _("Full Name"),
         null=False,
         blank=False,
         max_length=100,
+        help_text=_("Full Name as written naturally. Max 100 char."),
     )
-    non_latin_name = models.CharField(
+    non_latin_full_name = models.CharField(
         _("Non Latin Name"),
         null=False,
         blank=True,
         max_length=100,
+        help_text=_("Full Name in non latin characters. Blank is ok. Max 100 char."),
     )
     x_handle = models.CharField(
         _("X Handle"),
         null=False,
         blank=True,
         max_length=100,
+        help_text=_("Blank is ok. Max 100 char."),
     )
     avatar_url = models.URLField(
         _("Avatar URL"),
         null=False,
         blank=True,
         max_length=255,
+        help_text=_("Blank is ok. Max 255 char."),
     )
     notes = models.TextField(
-        _("Notes"),
-        null=False,
-        blank=True,
+        _("Notes"), null=False, blank=True, help_text=_("Blank is ok.")
     )
 
     def __str__(self) -> str:
-        return self.name
+        return self.full_name
