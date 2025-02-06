b244883f58e7169dc9456c1677a7d5f804baad3a
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-02-06 15:42:43 +0900

    Abstract time stamp model created in core app to be used throughout app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    created and modified fields
+    """
+
+    created = models.DateTimeField(
+        _("created"),
+        auto_now_add=True,
+    )
+    modified = models.DateTimeField(
+        _("modified"),
+        auto_now=True,
+    )
+
+    class Meta:
+        abstract = True
