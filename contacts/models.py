6570995c2c31fe83c94928dcaf2b408ca82b2c9b
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-02-06 16:20:07 +0900

    Basic contact model setup

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from core.models import TimeStampedModel
+
+
+class Contact(TimeStampedModel):
+    """
+    Base Model for contacts to be stored in
+    """
+
+    name = models.CharField(
+        _("Name"),
+        null=False,
+        blank=False,
+        max_length=100,
+    )
+    non_latin_name = models.CharField(
+        _("Non Latin Name"),
+        null=False,
+        blank=True,
+        max_length=100,
+    )
+    x_handle = models.CharField(
+        _("X Handle"),
+        null=False,
+        blank=True,
+        max_length=100,
+    )
+    avatar_url = models.URLField(
+        _("Avatar URL"),
+        null=False,
+        blank=True,
+        max_length=255,
+    )
+    notes = models.TextField(
+        _("Notes"),
+        null=False,
+        blank=True,
+    )
+
+    def __str__(self) -> str:
+        return self.name
