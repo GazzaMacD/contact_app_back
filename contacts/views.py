ff7d219b58e8294e01cca295666550573766a33b
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-02-07 10:15:37 +0900

    Basic contacts view and url created

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from contacts.models import Contact
+from contacts.serializers import ContactSerializer
+
+
+class ContactList(APIView):
+    """
+    List all contacts or create new contact
+    """
+
+    def get(self, request, format=None):
+        contacts = Contact.objects.all()
+        serializer = ContactSerializer(contacts, many=True)
+        return Response(serializer.data)
