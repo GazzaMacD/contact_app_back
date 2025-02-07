d9dacf7541a68909630f14b9637275350201adc8
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-02-07 10:33:09 +0900

    Add contact admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from contacts.models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Contact, ContactAdmin)
