380b681204d353ea7a51ba32a1a40d892b4d86dc
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-02-26 09:34:34 +0900

    Contact detail view and url, only get at present

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,6 +5,7 @@ from contacts import views
 
 urlpatterns = [
     path("contacts/", views.ContactList.as_view()),
+    path("contacts/<uuid:pub_id>/", views.ContactDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- contacts/views.py
+++ contacts/views.py
@@ -16,3 +16,20 @@ class ContactList(APIView):
         contacts = Contact.objects.all()
         serializer = ContactSerializer(contacts, many=True)
         return Response(serializer.data)
+
+
+class ContactDetail(APIView):
+    """
+    Get, update or delete a contact
+    """
+
+    def get_object(self, pub_id):
+        try:
+            return Contact.objects.get(pub_id=pub_id)
+        except Contact.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pub_id, format=None):
+        snippet = self.get_object(pub_id)
+        serializer = ContactSerializer(snippet)
+        return Response(serializer.data)
